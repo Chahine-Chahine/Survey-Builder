92d2ff3cdfc1752c954c727cea9262207e9fe681
Chahine-Chahine <chahinesquare@hotmail.com>
2023-12-20 04:25:07 +0200

    [core]: implemented CRUD operations on the survey

--- Backend/controllers/survey.controllers.js
+++ Backend/controllers/survey.controllers.js
@@ -1,4 +1,6 @@
 const Survey = require('../models/survey.schema');
+
+// Add Survey
 exports.addSurvey = async (req, res) => {
   try {
     const { title, adminId, questions } = req.body;
@@ -22,6 +24,71 @@ exports.addSurvey = async (req, res) => {
     });
   } catch (error) {
     console.error('Error creating survey:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+// Get all Surveys
+exports.getAllSurveys = async (req, res) => {
+  try {
+    const surveys = await Survey.find();
+    res.status(200).json({ surveys });
+  } catch (error) {
+    console.error('Error fetching surveys:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+// Get Survey by ID
+exports.getSurveyById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const survey = await Survey.findById(id);
+    if (!survey) {
+      return res.status(404).json({ error: 'Survey not found' });
+    }
+    res.status(200).json({ survey });
+  } catch (error) {
+    console.error('Error fetching survey by ID:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+// Update Survey
+exports.updateSurvey = async (req, res) => {
+  const { id } = req.params;
+  const { title, adminId, questions } = req.body;
+
+  try {
+    const updatedSurvey = await Survey.findByIdAndUpdate(
+      id,
+      { title, adminId, questions },
+      { new: true }
+    );
+
+    if (!updatedSurvey) {
+      return res.status(404).json({ error: 'Survey not found' });
+    }
+
+    res.status(200).json({ survey: updatedSurvey });
+  } catch (error) {
+    console.error('Error updating survey:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+// Delete Survey
+exports.deleteSurvey = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedSurvey = await Survey.findByIdAndDelete(id);
+    if (!deletedSurvey) {
+      return res.status(404).json({ error: 'Survey not found' });
+    }
+    res.status(204).json(); // No content for successful deletion
+  } catch (error) {
+    console.error('Error deleting survey:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
--- Backend/routes/survey.routes.js
+++ Backend/routes/survey.routes.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const { addSurvey } = require("../controllers/survey.controllers");
 const router = express.Router();
+const {
+  addSurvey,
+  getAllSurveys,
+  getSurveyById,
+  updateSurvey,
+  deleteSurvey,
+} = require('../controllers/survey.controllers');
 
-
-router.post("/survey" , addSurvey);
-
+router.post('/survey', addSurvey);
+router.get('/survey', getAllSurveys);
+router.get('/survey/:id', getSurveyById);
+router.put('/survey/:id', updateSurvey);
+router.delete('/survey/:id', deleteSurvey);
 
 module.exports = router;
