1a11cbd2056060f8249e5760df20f3afb3d53cb9
Chahine-Chahine <chahinesquare@hotmail.com>
2023-12-21 07:30:31 +0200

    [core]: implemented the fetch on the user side with bugs in routing

--- Frontend/src/pages/User/index.jsx
+++ Frontend/src/pages/User/index.jsx
@@ -19,3 +19,52 @@ function UserPage(){
 }
 
 export default UserPage;
+
+
+// Fetch with bugs
+
+
+// import { useState, useEffect } from "react";
+// import SurveyCard from "../../components/User/surveyCard";
+// import request from "../../helpers/request_helper"; 
+
+// function UserPage() {
+//   const [surveys, setSurveys] = useState([]);
+
+//   useEffect(() => {
+//     const fetchSurveys = async () => {
+//       try {
+//         const response = await request({
+//           route: "/", 
+//           method: "GET",
+//         });
+
+//         if (response.status === 200) {
+//           setSurveys(response.data.surveys);
+//           console.log('Surveys fetched successfully');
+//         } else {
+//           console.error('Failed to fetch surveys');
+//         }
+//       } catch (error) {
+//         console.error('An error occurred while fetching surveys:', error);
+//       }
+//     };
+
+//     fetchSurveys();
+//   }, []); 
+
+//   return (
+//     <>
+//       {surveys.map((survey) => (
+//         <SurveyCard
+//           key={survey._id}
+//           id={survey._id}
+//           Title={survey.title}
+//           Question={survey.questions.length > 0 ? survey.questions[0].text : ""}
+//         />
+//       ))}
+//     </>
+//   );
+// }
+
+// export default UserPage;
